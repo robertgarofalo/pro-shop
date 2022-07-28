1c9faf98c57aa0bee826be99db7ca4a3ebbf4db2
Robert <r.s.garofalo87@gmail.com>
2022-07-28 10:46:50 +1000

    add GET routes for all and single products and setup error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import products from './data/products.js'
 import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
 connectDB()
 const app = express()
 
-app.get('/api/products', (req, res) => {
-    res.send(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.send(product)
-})
+// error middleware
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
